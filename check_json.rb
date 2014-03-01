e03ca9191396d2ffea253f13e72ef7928d289d73
rsletten <rsletten@gmail.com>
2014-02-28 23:58:54 -0500

    modified script a bit

--- check_json.rb
+++ check_json.rb
@@ -5,10 +5,15 @@ require 'rubygems'
 require 'json'
 
 # variables
-@host = '10.40.20.15'
-@port = '8080'
+@host = ARGV[0] 
+@port = ARGV[1]
 @post = '/camstar-falcon-analysis/rest/search/event'
 @payload = File.read('dellrequest.txt') 
+# Nagios Exit codes
+STATE_OK = '0'
+STATE_WARNING = '1'
+STATE_CRITICAL = '2'
+STATE_UNKNOWN = '3'
 
 
 def post
@@ -20,17 +25,17 @@ end
 
 parsed = JSON.parse(post)
 
-timedOut =  parsed.fetch("timedOut")
-hasError =  parsed.fetch("hasError")
+timedOut = parsed.fetch("timedOut")
+hasError = parsed.fetch("hasError")
 
 
 if timedOut.to_s == 'true'
   puts 'timedOut is true'
-  exit 1
+  exit STATE_CRITICAL
 elsif hasError.to_s == 'true'
   puts 'hasError is true'
-  exit 1
+  exit STATE_WARNING
 else
   puts 'Check Passed'
-  exit 0
+  exit STATE_OK
 end
