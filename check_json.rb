e3ef8dcf1d3c5ca0388a377d5c47cf4f0d76a3c7
Rob Sletten <rsletten@gmail.com>
2014-03-05 08:46:37 -0500

    made improvements to check script

--- check_json.rb
+++ check_json.rb
@@ -10,9 +10,9 @@ require 'json'
 @post = '/camstar-falcon-analysis/rest/search/event'
 
 if ARGV[2] == 'dell'
-   @payload = File.read('dellrequest.txt')
+   @payload = File.read('/nagios/libexec/dellrequest.txt')
 elsif ARGV[2] == 'camstar' 
-   @payload = File.read('camstarrequest.txt')
+   @payload = File.read('/nagios/libexec/camstarrequest.txt')
 else
   puts "specify which tenant"
   exit 3
@@ -29,6 +29,9 @@ def post
     req.body = @payload
     response = Net::HTTP.new(@host, @port).start {|http| http.request(req) }
     return response.body
+rescue Exception => e
+  puts "Can't connect to " + @host.to_s + ":" + @port.to_s + ". Perhaps jetty is down?"
+  exit 2
 end
 
 parsed = JSON.parse(post)
